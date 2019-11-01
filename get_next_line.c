0d412edb29241afe06de3421e4695ee7d8a97c7c
Andreas Murtonen <Andreas@Andreas-MacBook-Pro.local>
2019-11-01 16:46:14 +0200

    rough draft of get next line

--- get_next_line.c
+++ get_next_line.c
@@ -14,5 +14,25 @@
 
 int		get_next_line(const int fd, char **line)
 {
-
+  static char	*str[MAX_FD];
+  char 		*temp;
+  char		buf[BUFF_SIZE + 1];
+  int		ret;
+  
+  if (fd < 0 || line == NULL)
+    return (-1);
+  while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+    {
+      buf[ret] = '\0';
+      tmp = ft_strjoin(str[fd], buf);
+      free(str[fd]);
+      str[fd] = tmp;
+      if (strchr(str[fd] == '\n'))
+	break;
+    }
+  if (ret < 0)
+    return (-1);
+  else if (ret == 0 && str[fd] == NULL || str[fd][0] == '\0')
+    return (0);
+  return (1);
 }
