9223f5453ce25c01dd5b51f7c85e28413dc157e0
Andreas Murtonen <amurtone@c2r3p4.hive.fi>
2019-11-04 15:29:57 +0200

    draft 2

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,60 @@
 /*   By: amurtone <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 15:38:18 by amurtone          #+#    #+#             */
-/*   Updated: 2019/10/29 15:39:41 by amurtone         ###   ########.fr       */
+/*   Updated: 2019/11/04 15:28:30 by amurtone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		get_next_line(const int fd, char **line)
+static int		get_line(char **str, char **line, const int fd)
 {
-  static char	*str[MAX_FD];
-  char 		*temp;
-  char		buf[BUFF_SIZE + 1];
-  int		ret;
-  
-  if (fd < 0 || line == NULL)
-    return (-1);
-  while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
-    {
-      buf[ret] = '\0';
-      tmp = ft_strjoin(str[fd], buf);
-      free(str[fd]);
-      str[fd] = tmp;
-      if (strchr(str[fd] == '\n'))
-	break;
-    }
-  if (ret < 0)
-    return (-1);
-  else if (ret == 0 && str[fd] == NULL || str[fd][0] == '\0')
-    return (0);
-  return (1);
+	int		i;
+	char	*tmp;
+
+	i = 0;
+	while (str[fd][i] != '\n' && str[fd][i] != '\0')
+		i++;
+	if (str[fd][i] == '\n')
+	{
+		*line = ft_strsub(str[fd], 0, i);
+		tmp = ft_strdup(str[fd] + (i + 1));
+		free(str[fd]);
+		str[fd] = tmp;
+		if (str[fd][0] == '\0')
+			ft_strdel(str);
+	}
+	else
+	{
+		*line = ft_strdup(str[fd]);
+		ft_strdel(str);
+	}
+	return (1);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static char		*str[MAX_FD];
+	char			*tmp;
+	char			buf[BUFF_SIZE + 1];
+	int				ret;
+
+	if (fd < 0 || fd > MAX_FD || !line)
+		return (-1);
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	{
+		buf[ret] = '\0';
+		if (str[fd] == NULL)
+			str[fd] = ft_strnew(1);
+		tmp = ft_strjoin(str[fd], buf);
+		free(str[fd]);
+		str[fd] = tmp;
+		if (ft_strchr(str[fd], '\n'))
+			break ;
+	}
+	if (ret < 0)
+		return (-1);
+	else if (ret == 0 && (str[fd] == NULL || str[fd][0] == '\0'))
+		return (0);
+	return (get_line(str, line, fd));
 }
---
+++ main.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amurtone <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/04 09:53:04 by amurtone          #+#    #+#             */
+/*   Updated: 2019/11/04 13:36:21 by amurtone         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+
+int		get_next(char *path)
+{
+	int		fd;
+	char	*line;
+	int		result;
+
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+	{
+		ft_putstr("Error in opening the file: ");
+		ft_putendl(path);
+		return (1);
+	}
+	while ((result = get_next_line(fd, &line)))
+	{
+		if (result == -1)
+		{
+			ft_putstr("Error getting next line in: ");
+			ft_putendl(path);
+			return (1);
+		}
+		ft_putendl(line);
+	}
+	if (close(fd) == -1)
+	{
+		ft_putstr("Error closing file: ");
+		ft_putendl(path);
+		return (1);
+	}
+	return (0);
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 1;
+	if (argc < 2)
+		ft_putendl("Needs at least one file");
+	while (i < argc)
+	{
+		if (get_next(argv[i++]))
+			return (1);
+	}
+	while (1)
+	{
+
+	}
+	return (0);
+}
